7eb933884411cd56e29204568296161a09c3ccc3
Mustafa Tarık KILIÇ <kilicmustafatarik@gmail.com>
2025-05-16 20:59:49 +0300

    Son değişiklikler kaydedildi.

---
+++ 3. Çıktı Alma/app.js
@@ -0,0 +1,17 @@
+// document.write ile çıktı alma. Alma diyorum çünkü artık kullanılmıyor.
+
+/*
+document.writeln("Hello World");
+document.writeln("Ben her şeyi öğrenmek istiyorum.");
+*/
+//Console.log() kullanılır.
+
+/*
+console.log("Selam yavrum");
+console.log("Ben Tarık");
+console.log("Buraya java kursundan geldim.");
+console.log ("Ben bu işi sevdim.");
+console.log("Benim bu klavye eklentilerini çok iyi kullanmam gerekiyor. Bunun için de iyi öğrenmeliyim kullandığım IDE'yi");
+ */
+
+//console.clear();
